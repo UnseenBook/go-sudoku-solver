e545b2f0eeb95d0025e5b7cfdd59005ef198e09b
Daan Schoone <daanschoone@gmail.com>
2019-03-03 11:56:11 +0100

    Make reading input more flexible

--- pkg/input/input.go
+++ pkg/input/input.go
@@ -2,48 +2,60 @@ package input
 
 import (
 	"bufio"
-	"errors"
 	"fmt"
 	"os"
 )
-
-func readLine() (string, error) {
-	in := bufio.NewReader(os.Stdin)
-	line, err := in.ReadString('\n')
-	if err != nil {
-		fmt.Println(err)
-		return "", err
-	}
-	if len(line) != 10 {
-		return "", errors.New("input a line with 9 characters, no more, no less")
-	}
-
-	return line, nil
-}
 
 // GetSudokuBoard reads console input and build the board from it
 func GetSudokuBoard() ([9][9]int, error) {
 	var boardInput [9][9]int
 
-	for count := 0; count < 9; count++ {
-		line, err := readLine()
-		if err != nil {
-			return boardInput, err
-		}
-
-		for index, value := range line {
-			if index == 9 {
-				break
-			}
-			if value == ' ' {
-				continue
-			}
-			if value < '0' || value > '9' {
-				return boardInput, errors.New("only fill in numbers 0-9 and spaces")
-			}
-			boardInput[count][index] = int(value - '0')
-		}
+	allInput, err := readAllInput()
+	if err != nil {
+		return boardInput, err
+	}
+	for i := 0; i < 9; i++ {
+		startInput := i * 9
+		copy(boardInput[i][:], allInput[startInput:startInput+9])
 	}
 
 	return boardInput, nil
+}
+
+func readAllInput() ([]int, error) {
+	in := bufio.NewReader(os.Stdin)
+	var input []int
+	for len(input) < 81 {
+		line, err := in.ReadString('\n')
+		if err != nil {
+			fmt.Println(err)
+			return nil, err
+		}
+
+		input = mergeInputs(input, getNumbersFromLine(line))
+	}
+
+	return input, nil
+}
+
+func mergeInputs(left []int, right []int) []int {
+	list := make([]int, len(left)+len(right))
+	copy(list, left)
+	copy(list[len(left):], right)
+
+	return list
+}
+
+func getNumbersFromLine(line string) []int {
+	var numbers []int
+	for _, value := range line {
+		if value == ' ' {
+			numbers = append(numbers, 0)
+		}
+		if value >= '0' && value <= '9' {
+			numbers = append(numbers, int(value-'0'))
+		}
+	}
+
+	return numbers
 }
